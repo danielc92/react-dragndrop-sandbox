62694d4a29505252666dbc4896362c2852db8703
Daniel Corcoran <danielc.92@outlook.com>
2020-07-18 10:54:52 +1000

    Added column dragged style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React, { useState } from 'react';
-import { initialData } from 'data';
-import { DropResult, ResponderProvided, Droppable, DragDropContext, Draggable } from 'react-beautiful-dnd';
+import React, { useState } from "react";
+import { initialData } from "data";
+import {
+  DropResult,
+  ResponderProvided,
+  Droppable,
+  DragDropContext,
+  Draggable,
+} from "react-beautiful-dnd";
+import { STYLES } from "styles";
 
 function App() {
   const [data, setData] = useState<DataState>(initialData);
@@ -47,58 +54,65 @@ function App() {
 
   return (
     <div style={STYLES.CONTAINER}>
-    <h1>Dans Drag n' Drop</h1>
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div style={{ display: "flex" }}>
-        {Object.entries(data).map((item) => (
-          <Droppable direction="vertical" droppableId={item[0]}>
-            {(provided, snapshot) => {
-              const computedCardStyle =
-                item[0] === "BACKLOG"
-                  ? STYLES.CARD_BACKLOG
-                  : item[0] === "COMPLETED"
-                  ? STYLES.CARD_COMPLETE
-                  : STYLES.CARD_IN_PROGRESS;
-              return (
-                <div
-                  {...provided.droppableProps}
-                  ref={provided.innerRef}
-                  style={STYLES.COLUMN}
-                >
-                  <h2>{item[0]}</h2>
-                  {data[item[0]].items.map((x, index) => (
-                    <Draggable
-                      key={x.itemId}
-                      draggableId={x.itemId.toString()}
-                      index={index}
-                    >
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                          style={{
-                            ...computedCardStyle,
-                            ...provided.draggableProps.style,
-                          }}
-                        >
-                          <h3 style={STYLES.CARD_HEADER}>
-                            {" "}
-                            {x.itemName} {x.itemId}
-                          </h3>
-                          <p style={STYLES.CARD_DESC}>{x.itemDescription}</p>
-                        </div>
-                      )}
-                    </Draggable>
-                  ))}
-                </div>
-              );
-            }}
-          </Droppable>
-        ))}
-      </div>
-    </DragDropContext>
-  </div>
+      <h1>Dans Drag n' Drop</h1>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div style={{ display: "flex" }}>
+          {Object.entries(data).map((item) => (
+            <Droppable direction="vertical" droppableId={item[0]}>
+              {(provided, snapshot) => {
+                const computedCardStyle =
+                  item[0] === "BACKLOG"
+                    ? { ...STYLES.CARD_BASE, ...STYLES.CARD_BACKLOG }
+                    : item[0] === "COMPLETED"
+                    ? { ...STYLES.CARD_BASE, ...STYLES.CARD_IN_PROGRESS }
+                    : { ...STYLES.CARD_BASE, ...STYLES.CARD_COMPLETE };
+
+                const additionalColumnStyle = snapshot.isDraggingOver
+                  ? {
+                      backgroundColor: "#c0e7f7",
+                    }
+                  : { backgroundColor: "#f4f4f4" };
+
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{ ...STYLES.COLUMN, ...additionalColumnStyle }}
+                  >
+                    <h2>{item[0]}</h2>
+                    {data[item[0]].items.map((x, index) => (
+                      <Draggable
+                        key={x.itemId}
+                        draggableId={x.itemId.toString()}
+                        index={index}
+                      >
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={{
+                              ...computedCardStyle,
+                              ...provided.draggableProps.style,
+                            }}
+                          >
+                            <h3 style={STYLES.CARD_HEADER}>
+                              {" "}
+                              {x.itemName} {x.itemId}
+                            </h3>
+                            <p style={STYLES.CARD_DESC}>{x.itemDescription}</p>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  </div>
+                );
+              }}
+            </Droppable>
+          ))}
+        </div>
+      </DragDropContext>
+    </div>
   );
 }
 
