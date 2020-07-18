6d2976d9a1495db27963b9db31e0db41069069d6
Daniel Corcoran <danielc.92@outlook.com>
2020-07-18 10:24:15 +1000

    Added state and utility functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { initialData } from 'data';
+import { DropResult, ResponderProvided } from 'react-beautiful-dnd';
 
 function App() {
+  const [data, setData] = useState<DataState>(initialData);
+  const handleDragEnd = (result: DropResult, provided: ResponderProvided) => {
+    // Wasnt dropped on column
+    if (!result.destination) return;
+
+    const { source, destination } = result;
+    // Was dropped in a variant column
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = data[source.droppableId];
+      const destColumn = data[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+
+      setData({
+        ...data,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems,
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems,
+        },
+      });
+    } else {
+      // The droppableId match -> reordered in same column
+      const column = data[source.droppableId];
+      const items = [...column.items];
+      const [removed] = items.splice(source.index, 1);
+      items.splice(destination.index, 0, removed);
+      setData({
+        ...data,
+        [source.droppableId]: {
+          ...column,
+          items,
+        },
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
     </div>
   );
 }
