8a509e932bac0cb3caa4f50e02f175d4daf3a3f6
Daniel Corcoran <danielc.92@outlook.com>
2020-07-18 10:27:29 +1000

    Add styles

---
+++ src/styles/index.ts
@@ -0,0 +1,36 @@
+export const STYLES = {
+    CARD_BACKLOG: {
+      backgroundColor: "#545454",
+      color: "white",
+      padding: "24px",
+      marginBottom: "16px",
+    },
+    CARD_COMPLETE: {
+      backgroundColor: "#4b62e5",
+      color: "white",
+      padding: "24px",
+      marginBottom: "16px",
+    },
+    CARD_IN_PROGRESS: {
+      backgroundColor: "#1bb77e",
+      color: "white",
+      padding: "24px",
+      marginBottom: "16px",
+    },
+    CARD_HEADER: {
+      margin: "0 0 12px 0",
+    },
+    CARD_DESC: {
+      margin: 0,
+    },
+    COLUMN: {
+      width: "300px",
+      background: "#f2f2f2",
+      padding: "16px",
+      marginRight: "16px",
+    },
+    CONTAINER: {
+      maxWidth: "1000px",
+      margin: "0 auto",
+    },
+  };
