3965aec459e493f0027f23811d5f5cb848a5036b
Daniel Corcoran <danielc.92@outlook.com>
2020-07-18 10:27:40 +1000

    Complete render

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { initialData } from 'data';
-import { DropResult, ResponderProvided } from 'react-beautiful-dnd';
+import { DropResult, ResponderProvided, Droppable, DragDropContext, Draggable } from 'react-beautiful-dnd';
 
 function App() {
   const [data, setData] = useState<DataState>(initialData);
@@ -46,8 +46,59 @@ function App() {
   };
 
   return (
-   <div>
-    </div>
+    <div style={STYLES.CONTAINER}>
+    <h1>Dans Drag n' Drop</h1>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <div style={{ display: "flex" }}>
+        {Object.entries(data).map((item) => (
+          <Droppable direction="vertical" droppableId={item[0]}>
+            {(provided, snapshot) => {
+              const computedCardStyle =
+                item[0] === "BACKLOG"
+                  ? STYLES.CARD_BACKLOG
+                  : item[0] === "COMPLETED"
+                  ? STYLES.CARD_COMPLETE
+                  : STYLES.CARD_IN_PROGRESS;
+              return (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  style={STYLES.COLUMN}
+                >
+                  <h2>{item[0]}</h2>
+                  {data[item[0]].items.map((x, index) => (
+                    <Draggable
+                      key={x.itemId}
+                      draggableId={x.itemId.toString()}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={{
+                            ...computedCardStyle,
+                            ...provided.draggableProps.style,
+                          }}
+                        >
+                          <h3 style={STYLES.CARD_HEADER}>
+                            {" "}
+                            {x.itemName} {x.itemId}
+                          </h3>
+                          <p style={STYLES.CARD_DESC}>{x.itemDescription}</p>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                </div>
+              );
+            }}
+          </Droppable>
+        ))}
+      </div>
+    </DragDropContext>
+  </div>
   );
 }
 
